fd6e3705437d0f7e5f5bcb2723666740b792705b
TMN <tommymartin1234@gmail.com>
2024-05-28 09:01:50 -0600

    init

---
+++ app/components/AppShell.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { AppShell as MantineAppShell, Burger, Group } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export const AppShell = ({ children }: { children: ReactNode }) => {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <MantineAppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <MantineAppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <div className="flex justify-around w-full gap-4">
+            <Link className="mr-auto" href={"/"}>
+              Tommy MARTIN
+            </Link>
+            <Link href="https://github.com/mlfcnt" target="_blank">
+              Github
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/tommy-martin-72956b1ab/"
+              target="_blank"
+            >
+              Linkedin
+            </Link>
+          </div>
+        </Group>
+      </MantineAppShell.Header>
+      <MantineAppShell.Navbar p="md">
+        <Link href={"/"}>Home</Link>
+        <Link href={"why"}>Why did I make this?</Link>
+        <Link href={"/code"}>Code highlights</Link>
+      </MantineAppShell.Navbar>
+      <MantineAppShell.Main>{children}</MantineAppShell.Main>
+    </MantineAppShell>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AppShell } from "./components/AppShell";
+import "@mantine/core/styles.css";
+
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+
+      <body className={inter.className}>
+        <MantineProvider>
+          <AppShell>{children}</AppShell>
+        </MantineProvider>
+      </body>
     </html>
   );
 }
